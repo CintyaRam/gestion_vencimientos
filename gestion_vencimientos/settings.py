7051b07db7f8a219880c3928c49ddda12c1069d3
Cintya Ramirez <cintya.ramirezbarriga@gmail.com>
2025-11-23 16:51:42 -0300

    Config settings admin

--- gestion_vencimientos/settings.py
+++ gestion_vencimientos/settings.py
@@ -144,6 +144,8 @@ if os.environ.get("RAILWAY_ENVIRONMENT"):
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 else:
     STATICFILES_DIRS = [ BASE_DIR / "static" ]
+
+    
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
